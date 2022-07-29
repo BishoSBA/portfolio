bf9a1b56bf6d437e9abb947a058577b23416f0f3
Beshir Saeed <Beshir-saeed@live.com>
2022-07-29 23:07:03 +0300

    add animation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import styles from "./style.scss";
 
-console.log("hi");
+// Languages
+
+const stack1 = document.getElementById("stack1");
+const stack2 = document.getElementById("stack2");
+const stack3 = document.getElementById("stack3");
+
+const viewPortHeight = document.documentElement.clientHeight;
+
+document.addEventListener("scroll", onScroll);
+
+const stacks = [stack1, stack2, stack3];
+
+// Projects
+
+let projects = document.querySelectorAll(".project");
+
+projects = [...projects];
+
+projects = projects.filter((project) => project.nodeType === 1);
+
+function onScroll() {
+	stacks.forEach((language) => {
+		if (language.getBoundingClientRect().top < viewPortHeight) {
+			language.style.animation = "hero 800ms ease-in-out forwards";
+		}
+	});
+
+	projects.forEach((project) => {
+		if (project.getBoundingClientRect().top < viewPortHeight) {
+			project.style.animation = "hero 800ms ease-in-out forwards";
+		}
+	});
+}
